ae73bad88e73366f3ed6dca6b9aaae043fc41dbf
Sean Michael Shea <seanshea1337@gmail.com>
2021-09-03 10:59:45 -0400

    and useEffect to call recipe and create useState for recipe food query

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 import Results from "../Results/Results";
 
 const Recipe = () => {
+	const [foodQuery, setFoodQuery] = useState("pizza");
+
 	useEffect(() => {
 		getRecipe();
 	}, []);
 
 	const getRecipe = async () => {
-		const apiURL = `${process.env.REACT_APP_Recipe_Acount_URL}&q=pizza&app_id=${process.env.REACT_APP_Recipe_Acount_KEY}&app_key=${process.env.REACT_APP_Recipe_API_KEY}`;
+		const apiURL = `${process.env.REACT_APP_Recipe_Acount_URL}&q=${foodQuery}&app_id=${process.env.REACT_APP_Recipe_Acount_KEY}&app_key=${process.env.REACT_APP_Recipe_API_KEY}`;
 		const response = await fetch(apiURL);
 		const data = await response.json();
 		console.log(data.hits);
