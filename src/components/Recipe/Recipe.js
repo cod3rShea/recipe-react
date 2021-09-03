f79a5c4b736c7ca9db56f0c6e6acf3b3537dedaf
Sean Michael Shea <seanshea1337@gmail.com>
2021-09-03 10:48:32 -0400

    Use New api and start building out env vars

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -2,37 +2,14 @@ import React, { useState } from "react";
 import Results from "../Results/Results";
 
 const Recipe = () => {
-	const [yourRecipe, setYourRecipe] = useState("");
-
-	const recipeCall = () => {
-		getIngredients();
-	};
-
-	async function getIngredients(recipe) {
-		const apiKey = "a366a414f28d41989a4cb9503589bb62";
-		const food = yourRecipe;
-		const INGREDIENTS_URL = `https://api.spoonacular.com/recipes/search?apiKey=${apiKey}&query=${food}`;
-		const res = await fetch(INGREDIENTS_URL, {
-			method: "GET",
-			body: JSON.stringify(recipe),
-			headers: {
-				"Content-Type": "application/x-www-form-urlencoded",
-			},
-		});
-		//console.log(res);
-		const data = await res.json();
-		console.log(data);
-	}
+	const apiURL = `${process.env.REACT_APP_Recipe_Acount_URL}&q=pizza&app_id=${process.env.REACT_APP_Recipe_Acount_KEY}&app_key=${process.env.REACT_APP_Recipe_API_KEY}`;
 
 	return (
 		<div>
-			<input
-				name="yourRecipe"
-				value={yourRecipe}
-				placeholder="Look Up Food"
-				onChange={(e) => setYourRecipe(e.target.value)}
-			/>
-			<button onClick={recipeCall}>Test</button>
+			<form>
+				<input />
+				<button />
+			</form>
 		</div>
 	);
 };
