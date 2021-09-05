e1370fd4df9c724488350419a3d2bdba51188be9
Sean Michael Shea <seanshea1337@gmail.com>
2021-09-05 10:06:54 -0400

    add function for submit and function to gather text from input

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from "react";
 import Results from "../Results/Results";
 
 const Recipe = () => {
-	const [foodQuery, setFoodQuery] = useState("pizza");
 	const [recipes, setRecipes] = useState([]);
+	const [recipeSearch, setRecipeSearch] = useState("");
+	const [foodQuery, setFoodQuery] = useState("");
 
 	useEffect(() => {
 		getRecipe();
-	}, []);
+	}, [foodQuery]);
 
 	const getRecipe = async () => {
 		const apiURL = `${process.env.REACT_APP_Recipe_Acount_URL}&q=${foodQuery}&app_id=${process.env.REACT_APP_Recipe_Acount_KEY}&app_key=${process.env.REACT_APP_Recipe_API_KEY}`;
@@ -16,14 +17,23 @@ const Recipe = () => {
 		setRecipes(data.hits);
 	};
 
+	const updateRecipeSearch = (e) => {
+		setRecipeSearch(e.target.value);
+	};
+	const getRecipeSearch = (e) => {
+		e.preventDefault();
+		setFoodQuery(recipeSearch);
+		setRecipeSearch("");
+	};
+
 	// renders first on page because foodQuery will be empty
 	if (foodQuery === "") {
 		return (
 			<div>
 				<h1>Search For A Recipe</h1>
-				<form>
-					<input />
-					<button> Find Recipe</button>
+				<form onSubmit={getRecipeSearch}>
+					<input value={recipeSearch} onChange={updateRecipeSearch} />
+					<button type="submit">Find Recipe</button>
 				</form>
 			</div>
 		);
@@ -31,9 +41,10 @@ const Recipe = () => {
 	//Renders on submit and passes recipes into results component
 	return (
 		<div>
-			<form>
-				<input />
-				<button> Find Recipe</button>
+			<h2>Below are you results for {foodQuery}</h2>
+			<form onSubmit={getRecipeSearch}>
+				<input value={recipeSearch} onChange={updateRecipeSearch} />
+				<button type="submit">Find Recipe</button>
 			</form>
 			<Results data={recipes} />
 		</div>
